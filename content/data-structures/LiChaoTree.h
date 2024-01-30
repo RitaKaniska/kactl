6bc98ba4a69e04f49a770754a723fb9d545d75d7
Vo Khac Trieu <72329092+DeMen100ns@users.noreply.github.com>
2024-01-30 15:08:55 +0700

    Update LiChaoTree.h

--- content/data-structures/LiChaoTree.h
+++ content/data-structures/LiChaoTree.h
@@ -1,52 +1,52 @@
 /**
- * Author: DeMen100ns
- * Date: 2023
+ * Author: Lamu
+ * Date: 2024
  * Description: 
  */
 
-struct LiChaoTree{
-    const int N = 3e5 + 5;
-    Line seg[4 * N];
-    void upd(int id, int l, int r, Line line){
-        if (l == r){
-            if (line(l) < seg[id](l)) seg[id] = line;
+struct node
+{
+    Line f;
+    node *l, *r;
+    node() {
+        f = {0,(int)1e18};
+        l = r = 0;
+    }
+};
+struct LiChaoTree
+{
+    node *tree;
+    void upd(node *&x, int lx, int rx, Line line) {
+        if (!x) x = new node;
+        if (lx == rx)
+        {
+            if (x->f(lx) > line(lx)) swap(x->f, line);
             return;
         }
-
-        int mid = (l + r) >> 1;
-
-        if (line.a < seg[id].a) swap(seg[id], line);
-
-        if (line(mid) < seg[id](mid)){
-            swap(seg[id], line);
-            upd(id << 1 | 1, mid + 1, r, line);
-        } else {
-            upd(id << 1, l, mid, line);
-        }
+        int mid = lx + (rx - lx) / 2;
+        if (x->f(mid) > line(mid)) swap(x->f, line);
+        if (x->f(lx) > line(lx))
+            upd(x->l, lx, mid, line);
+        else upd(x->r, mid+1, rx, line);
     }
 
-    int get(int id, int l, int r, int x){
-        if (l == r) return seg[id](x);
-
-        int ans, mid = (l + r) >> 1;
-        if (mid >= x){
-            ans = get(id << 1, l, mid, x);
-        } else {
-            ans = get(id << 1 | 1, mid + 1, r, x);
-        }
-
-        return min(ans, seg[id](x));
+    int get(node *&x, int lx, int rx, int k) {
+        if (!x) return 1e18;
+        if (lx == rx) return x->f(k);
+        int mid = lx + (rx - lx) / 2;
+        if (k <= mid) return min(x->f(k), get(x->l, lx, mid, k));
+        else return min(x->f(k), get(x->r, mid+1, rx, k));
     }
 
-    void updr(int id, int l, int r, int u, int v, Line line){
-        if (l > v || r < u || l > r) return;
-        if (l >= u && r <= v){
-            upd(id, l, r, line);
+    void updr(node *&x, int lx, int rx, int l, int r, Line line) {
+        if (lx > r || rx < l) return;
+        if (!x) x = new node;
+        if (lx >= l && rx <= r) {
+            upd(x, lx, rx, line);
             return;
         }
-
-        int mid = (l + r) >> 1;
-        updr(id << 1, l, mid, u, v, line);
-        updr(id << 1 | 1, mid + 1, r, u, v, line);
+        int mid = lx + (rx - lx) / 2;
+        updr(x->l, lx, mid, l, r, line);
+        updr(x->r, mid+1, rx, l, r, line);
     }
 };
