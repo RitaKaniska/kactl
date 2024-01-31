f39b5705906df3c7e224d71ebd2b69235b26efaa
Vo Khac Trieu <72329092+DeMen100ns@users.noreply.github.com>
2024-01-31 17:39:41 +0700

    Update SubsetConvolution.h

--- content/numerical/SubsetConvolution.h
+++ content/numerical/SubsetConvolution.h
@@ -7,41 +7,48 @@
  * - f o g(s') = sum(s in s') f(s) * g(s - s')
  */
 
-// Make fhat[][] = {0} and ghat[][] = {0}
-for(int mask = 0; mask < (1 << N); mask++) {
-    fhat[__builtin_popcount(mask)][mask] = f[mask];
-    ghat[__builtin_popcount(mask)][mask] = g[mask];
-}
+const int N = 20;
+base fhat[N + 1][1 << N], ghat[N + 1][1 << N], h[N + 1][1 << N];
+base f[1 << N], g[1 << N], fog[1 << N];
 
-// Apply zeta transform on fhat[][] and ghat[][]
-for(int i = 0; i < N; i++) {
-    for(int j = 0; j < N; j++) {
-        for(int mask = 0; mask < (1 << N); mask++) {
-            if((mask & (1 << j)) != 0) {
-                fhat[i][mask] += fhat[i][mask ^ (1 << j)];
-                ghat[i][mask] += ghat[i][mask ^ (1 << j)];
+void SOS_convolution(){
+    // Make fhat[][] = {0} and ghat[][] = {0}
+    for(int mask = 0; mask < (1 << N); mask++) {
+        fhat[__builtin_popcount(mask)][mask] = f[mask];
+        ghat[__builtin_popcount(mask)][mask] = g[mask];
+    }
+
+    // Apply zeta transform on fhat[][] and ghat[][]
+    for(int i = 0; i <= N; i++) {
+        for(int j = 0; j <= N; j++) {
+            for(int mask = 0; mask < (1 << N); mask++) {
+                if((mask & (1 << j)) != 0) {
+                    fhat[i][mask] += fhat[i][mask ^ (1 << j)];
+                    ghat[i][mask] += ghat[i][mask ^ (1 << j)];
+                }
             }
         }
     }
-}
 
-// Do the convolution and store into h[][] = {0}
-for(int mask = 0; mask < (1 << N); mask++) {
-    for(int i = 0; i < N; i++) {
-        for(int j = 0; j <= i; j++) {
-            h[i][mask] += fhat[j][mask] * ghat[i - j][mask];
-        } 
+    // Do the convolution and store into h[][] = {0}
+    for(int mask = 0; mask < (1 << N); mask++) {
+        for(int i = 0; i <= N; i++) {
+            for(int j = 0; j <= i; j++) {
+                h[i][mask] += fhat[j][mask] * ghat[i - j][mask];
+            } 
+        }
     }
-}
 
-// Apply inverse SOS dp on h[][]
-for(int i = 0; i < N; i++) {
-    for(int j = 0; j < N; j++) {
-        for(int mask = 0; mask < (1 << N); mask++) {
-            if((mask & (1 << j)) != 0) {
-                h[i][mask] -= h[i][mask ^ (1 << j)];
+    // Apply inverse SOS dp on h[][]
+    for(int i = 0; i <= N; i++) {
+        for(int j = 0; j <= N; j++) {
+            for(int mask = 0; mask < (1 << N); mask++) {
+                if((mask & (1 << j)) != 0) {
+                    h[i][mask] -= h[i][mask ^ (1 << j)];
+                }
             }
         }
     }
+    for(int mask = 0; mask < (1 << N); mask++)  fog[mask] = h[__builtin_popcount(mask)][mask];
 }
-for(int mask = 0; mask < (1 << N); mask++)  fog[mask] = h[__builtin_popcount(mask)][mask];
+
