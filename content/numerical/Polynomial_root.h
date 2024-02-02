8797eb071c2f680b2585f2f4c57b3a33e98fc56d
Vo Khac Trieu <72329092+DeMen100ns@users.noreply.github.com>
2024-02-02 21:14:39 +0700

    Create Polynomial_root.h

---
+++ content/numerical/Polynomial_root.h
@@ -0,0 +1,20 @@
+/**
+ * Author: ShahjalalShohag
+ * Date: 2023-11-30
+ * Description:
+ */
+
+#include "Polynomial.h"
+
+poly root(int n, int k = 2) const { //kth root of p(x) mod x^n
+  if(is_zero()) return *this;
+  if (k == 1) return mod_xk(n);
+  assert(a[0] == 1);
+  poly q({1});
+  for(int i = 1; i < n; i <<= 1){
+    if(k == 2) q += mod_xk(2 * i) * q.inverse(2 * i);
+    else q = q * mint(k - 1) + mod_xk(2 * i) * q.inverse(2 * i).pow(k - 1, 2 * i);
+    q = q.mod_xk(2 * i) / mint(k);
+  }
+  return q.mod_xk(n);
+}
